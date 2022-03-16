27474ca2cb317ea985e68967c7739b7705ac13f6
ourinkulov <63137610+ourinkulov@users.noreply.github.com>
2022-03-16 09:32:54 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Livewire\Kafedras;
 use App\Http\Livewire\Admin\Teachers\Teachers;
@@ -30,7 +28,7 @@ use App\Http\Livewire\Admin\Showallons\Showallons;
 use App\Http\Livewire\User\Showallmarks\Showallmarks;
 use App\Http\Livewire\Admin\Yakuniynazorats\Yakuniynazorats;
 use App\Http\Livewire\User\Ynjurnals\Ynjurnals;
-use App\Http\Controllers\StudentController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -53,9 +51,9 @@ Route::middleware(['middleware'=>'PreventBackHistory'])->group(function(){
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['prefix'=>'admin','middleware'=>['isAdmin','auth','PreventBackHistory']], function(){
-    Route::get('dashboard',[AdminController::class,'index'])->name('admin.dashboard');
-    Route::get('profile',[AdminController::class,'profile'])->name('admin.profile');
-    Route::get('settings',[AdminController::class,'settings'])->name('admin.settings');
+    Route::get('dashboard','AdminController@index')->name('admin.dashboard');
+    Route::get('profile','AdminController@profile')->name('admin.profile');
+    Route::get('settings','AdminController@settings')->name('admin.settings');
     Route::get('/kafedras',Kafedras::class);
     Route::get('/kafedras',Kafedras::class);
     Route::get('/teachers',Teachers::class);
@@ -76,9 +74,9 @@ Route::group(['prefix'=>'admin','middleware'=>['isAdmin','auth','PreventBackHist
 
 });
 Route::group(['prefix'=>'users','middleware'=>['isUser','auth','PreventBackHistory']],function (){
-    Route::get('dashboard',[UserController::class,'index'])->name('users.dashboard');
-    Route::get('profile',[UserController::class,'profile'])->name('users.profile');
-    Route::get('settings',[UserController::class,'settings'])->name('users.settings');
+    Route::get('dashboard','UserController@index')->name('users.dashboard');
+    Route::get('profile','UserController@profile')->name('users.profile');
+    Route::get('settings','UserController@settings')->name('users.settings');
     Route::get('/kurs',Kurs::class);
     Route::get('/quiz',Quiz::class);
     Route::get('/questions/{id}',Questions::class);
@@ -92,22 +90,11 @@ Route::group(['prefix'=>'users','middleware'=>['isUser','auth','PreventBackHisto
 });
 
 Route::group(['prefix'=>'student','middleware'=>['isStudent','auth','PreventBackHistory']],function (){
-   Route::get('dashboard',[StudentController::class,'index'])->name('student.dashboard');
+   Route::get('dashboard','StudentController@index')->name('student.dashboard');
 });
 
 
-Route::get('/chart',function (){
-   $users = \App\Models\User::select(DB::raw("COUNT(*) as count"))->whereYear('created_at',date('Y'))->groupBy(DB::raw('Month(created_at)'))->pluck('count');
-   $month = \App\Models\User::select(DB::raw("Month(created_at) as month"))->whereYear('created_at',date('Y'))->groupBy(DB::raw("Month(created_at)"))->pluck('month');
-   //dd(count($users));
-   $datas = array(0,0,0,0,0,0,0,0);
-   $username = \App\Models\User::all()->where('id','=',2)->first();
-   foreach ($month as $index=>$mon)
-   {
-       $datas[$mon-1] = $users[$index];
-   }
 
-   return view('chart',compact(['datas','username']));});
 //
 //Notification::route('telegram', '637399091')
 //    ->notify(new \App\Notifications\Telegram());
